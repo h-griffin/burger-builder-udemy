09719b1d1111093a83b2658eb6b8f9716d8c557c
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-09-16 14:11:25 -0700

    order summary and buttons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,29 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //tranform state object into array
+    let transformedIngredients = Object.keys(props.ingredients)//keys returns array / .map the array
+        .map(igKey => {                                          //igkey=bacon
+            //transform string key value into array with 2 elements
+            return[...Array(props.ingredients[igKey])]          // length of igkey = 2=[,]  /Array.(length)
+            .map((_, i) => {                                    //_empty argument, just want index of element
+                return <BurgerIngredient key={igKey + i} type={igKey} /> //key=bacon i=1 type='bacon'
+            })
+        })
+        .reduce( (arr, el) => {
+            return arr.concat(el) //ad new el to old arr
+        }, []); 
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>start adding ingredients!</p>
+    }
+
+
+    console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type ='bread-top'/>
-            <BurgerIngredient type ='cheese'/>
-            <BurgerIngredient type ='meat'/>
+            {transformedIngredients}
             <BurgerIngredient type ='bread-bottom'/>
         </div>
     );
