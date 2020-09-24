9a18d45d8233ffa46ce1efc218e52b6eda6720f7
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-09-24 15:53:50 -0700

    local host finished

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients) //expect obj convert to arr
+class OrderSummary extends Component{
+    //this could be a func component
+    componentDidUpdate(){
+        console.log('[order sumary] will update');
+    }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients) //expect obj convert to arr
         .map(igKey => {
             return <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}
                     </li>
         })
 
-    return (
-        <Aux>
+        return(<Aux>
             <h3>Your Order</h3>
             <p>a delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong> </p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong> </p>
             <p>continue to checkout?</p>
             <Button
                 btnType='Danger'
-                clicked={props.purchaseCancelled}
+                clicked={this.props.purchaseCancelled}
                 >CANCEL</Button>
             <Button
                 btnType='Success'
-                clicked={props.purchaseContinued}
+                clicked={this.props.purchaseContinued}
                 >CONTINUE</Button>
-        </Aux>
-    )
-}
-
-export default orderSummary;
+        </Aux>)
+    }
+};
+export default OrderSummary;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import classes from './/NavigationItems.css'
 import NavigationItem from './NavigationItem/NavigationItem';
 
 
-const navigationItems = (props) => (
+const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' active >BurgerBuilder</NavigationItem>
         <NavigationItem link='/'>Checkout</NavigationItem>
